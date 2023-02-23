cff4fa8c658ede29f499f3c8773e3d3141892baa
Dannie Vinther <davi@kea.dk>
2023-02-23 13:38:43 +0100

    cd

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
-import { animate, scroll } from "motion";
+import { animate, inView } from "motion";
 
-const el = document.querySelector(".element");
+// Create the query list.
+const mediaQueryList = window.matchMedia("(min-width: 1000px)");
 
-scroll(animate("h1", { x: 100 }), {
-  target: el,
-  offset: ["end", `-${el.clientHeight}px start`],
-});
+// Define a callback function for the event listener.
+function handleOrientationChange(evt) {
+  if (evt.matches) {
+    inviewAnimation();
+  } else {
+    // setTimeout(() => {
+    //   inviewAnimation2();
+    // }, 100);
+    requestAnimationFrame(() => {
+      inviewAnimation2();
+    });
+  }
+}
+
+// Run the orientation change handler once.
+handleOrientationChange(mediaQueryList);
+
+// Add the callback function as a listener to the query list.
+mediaQueryList.addEventListener("change", handleOrientationChange);
+
+const inviewAnimation = () => {
+  return inView(".mangeelementerinview", () => {
+    animate(".mangeelementer1", { x: [-2000, 200] }, { duration: 2 });
+    animate(".mangeelementer2", { x: [-1500, 0] }, { duration: 2 });
+    animate(".mangeelementer3", { x: [-1000, 0] }, { duration: 2 });
+    animate(".mangeelementer4", { x: [-500, 0] }, { duration: 2 });
+    animate(".mangeelementer5", { x: [-2000, 0] }, { duration: 2 });
+    animate(".mangeelementer6", { x: [2000, 0] }, { duration: 2 });
+    animate(".mangeelementer7", { x: [1500, 0] }, { duration: 2 });
+    animate(".mangeelementer8", { x: [1000, 0] }, { duration: 2 });
+    animate(".mangeelementer9", { x: [500, 0] }, { duration: 2 });
+    animate(".mangeelementer10", { x: [2000, 0] }, { duration: 2 });
+    return () => {
+      animate(".mangeelementer1", { x: [-2000, 0] }, { duration: 2 });
+      animate(".mangeelementer2", { x: [-1500, 0] }, { duration: 2 });
+      animate(".mangeelementer3", { x: [-1000, 0] }, { duration: 2 });
+      animate(".mangeelementer4", { x: [-500, 0] }, { duration: 2 });
+      animate(".mangeelementer5", { x: [-2000, 0] }, { duration: 2 });
+      animate(".mangeelementer6", { x: [2000, 0] }, { duration: 2 });
+      animate(".mangeelementer7", { x: [1500, 0] }, { duration: 2 });
+      animate(".mangeelementer8", { x: [1000, 0] }, { duration: 2 });
+      animate(".mangeelementer9", { x: [500, 0] }, { duration: 2 });
+      animate(".mangeelementer10", { x: [2000, 0] }, { duration: 2 });
+    };
+  });
+};
+
+const inviewAnimation2 = () => {
+  return inView(".mangeelementerinview", () => {
+    animate(".mangeelementer1", { x: [-2000, 200] }, { duration: 2 });
+    animate(".mangeelementer2", { x: [-1500, 100] }, { duration: 2 });
+    animate(".mangeelementer3", { x: [-1000, 0] }, { duration: 2 });
+    animate(".mangeelementer4", { x: [-500, 0] }, { duration: 2 });
+    animate(".mangeelementer5", { x: [-2000, 0] }, { duration: 2 });
+    animate(".mangeelementer6", { x: [2000, 0] }, { duration: 2 });
+    animate(".mangeelementer7", { x: [1500, 0] }, { duration: 2 });
+    animate(".mangeelementer8", { x: [1000, 0] }, { duration: 2 });
+    animate(".mangeelementer9", { x: [500, 0] }, { duration: 2 });
+    animate(".mangeelementer10", { x: [2000, 0] }, { duration: 2 });
+    return () => {
+      animate(".mangeelementer1", { x: [-2000, 0] }, { duration: 2 });
+      animate(".mangeelementer2", { x: [-1500, 0] }, { duration: 2 });
+      animate(".mangeelementer3", { x: [-1000, 0] }, { duration: 2 });
+      animate(".mangeelementer4", { x: [-500, 0] }, { duration: 2 });
+      animate(".mangeelementer5", { x: [-2000, 0] }, { duration: 2 });
+      animate(".mangeelementer6", { x: [2000, 0] }, { duration: 2 });
+      animate(".mangeelementer7", { x: [1500, 0] }, { duration: 2 });
+      animate(".mangeelementer8", { x: [1000, 0] }, { duration: 2 });
+      animate(".mangeelementer9", { x: [500, 0] }, { duration: 2 });
+      animate(".mangeelementer10", { x: [2000, 0] }, { duration: 2 });
+    };
+  });
+};
